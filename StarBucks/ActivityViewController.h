a5d674c34210ebacf828cfb5cf8eff9359701890
jy01902848 <jy01902848@gmail>
2012-05-15 20:55:50 +0800

    222

---
+++ StarBucks/ActivityViewController.h
@@ -0,0 +1,31 @@
+//
+//  ActivityViewController.h
+//  StarBucks
+//
+//  Created by 浩洋 朱 on 12-5-10.
+//  Copyright (c) 2012年 首正. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PagePhotosDataSource.h"
+#import "PagePhotosView.h"
+
+
+@interface ActivityViewController : UIViewController
+<PagePhotosDataSource,ActivityDelegate>{
+    UITableView *__weak m_tableView;
+    UISegmentedControl *__weak segment;
+    
+    PagePhotosView *m_pagePhotosView;
+    
+    NSMutableArray *arrImage;
+    UIButton *bigBt;
+}
+
+@property(nonatomic,weak) IBOutlet UITableView *m_tableView;
+@property(nonatomic,weak) IBOutlet UISegmentedControl *segment;
+@property(nonatomic,strong) PagePhotosView *m_pagePhotosView;
+
+-(void) clickedImage:(UIButton*)bt;
+//- (void)remove;
+@end
