7be8ce306e44955ad267073a843fb56bbeb52954
jy01902848 <jy01902848@gmail>
2012-05-15 21:00:57 +0800

    文档

--- StarBucks/HomePageViewController.h
+++ StarBucks/HomePageViewController.h
@@ -5,7 +5,7 @@
 //  Created by 浩洋 朱 on 12-5-10.
 //  Copyright (c) 2012年 首正. All rights reserved.
 //
-
+//  文档
 #import <UIKit/UIKit.h>
 #import "ActivityViewController.h"
 //#import "ShopSearchViewController.h"
